3d510dee2e4e7d22d2b75e16a59d8e5084fe9f5c
Harshit Jain <harshit_jain@intuit.com>
2020-03-24 20:26:20 +0530

    myth data updated

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,47 +1,129 @@
 export default
 [
       {
-        myth: "Avoid shipped packages/gas pumps/shopping carts/ATMs or you’ll die.",
-        truth: "Coronavirus surface survival is one thing; that surface causing an infection is another. Wash your hands; live your life"
+        myth: 'If you can hold your breath for more than 10 seconds without discomfort, there is no fibrosis or scarring in the lungs and basically no infection',
+        truth: 'Holding your breath is not a test for fibrosis and fibrosis is not a symptom of Covid-19. Main symptoms are high fever and persitent dry cough.',
+        link: 'https://www.bbc.com/news/av/51979410/coronavirus-more-myths-to-ignore',
+        mythBustedBy: 'Chris Morris - BBC News'
       },
       {
-        myth: " You can catch COVID-19 from ordering takeout food/Chinese food (or the packaging of food).",
-        truth: "COVID-19 is a droplet related infection (like flu) not a food-borne infection (like salmonella etc.). There is no documented COVID risk with take-out food."
+        myth: 'You can use homemade sanitisers by using disinfectants for cleaning surfaces or Vodka ',
+        truth: 'Anything based on disinfectants for cleaning surfaces will not be good for your skin. Also Vodka does not contain enough alcohol to be effective',
+        link: 'https://www.bbc.com/news/av/51979410/coronavirus-more-myths-to-ignore',
+        mythBustedBy: 'Chris Morris - BBC News'
       },
       {
-        myth: "Going into a sauna for 20 min can kill more than 90% of viruses, including coronavirus. ",
-        truth: "There are no scientific trials to suggest the validity of this claim. On the contrary, saunas can cause pneumonias, folliculitis, etc"
+        myth: 'The virus can survive on surfaces for up to a month.',
+        truth: 'This is highly unlikely. Take research on other strains of coronavirus like Sars or Mers. They tell that they can survive from about 2 hours up to a maximum of 9 days.',
+        link: 'https://www.bbc.com/news/av/51979410/coronavirus-more-myths-to-ignore',
+        mythBustedBy: 'Chris Morris - BBC News'
+      },
+      {
+        myth: 'Cow Urine has medicinal properties and can ward off coronavirus and other diseases.',
+        truth: 'There is no evidence at all that Cow urine can prevent or cure Covid-19 ',
+        link: 'https://www.bbc.com/news/av/51979410/coronavirus-more-myths-to-ignore',
+        mythBustedBy: 'Chris Morris - BBC News'
+      },
+      {
+        myth: 'Coronavirus cannot live in hot and humid areas. Thus when the temperature rises, coronavirus would be killed',
+        truth: 'The new Coronavirus can be transmitted in areas with hot and humid climate',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/52.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Coronavius cannot live in cold weather and snow. Thus people living in cold countries are safe',
+        truth: 'There is no reason to believe that cold weather can kill the new coronavirus or other diseases. The normal human body temperature remains around 36.5°C to 37°C, regardless of the external temperature or weather',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mb-cold-snow.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Taking a hot bath can help in prevention of Covid-19 as the hot water would kill the Coronavirus',
+        truth: 'Taking a hot bath will not prevent you from catching COVID-19. Your normal body temperature remains around 36.5°C to 37°C, regardless of the temperature of your bath or shower',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mb-hot-bath.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'The new Coronavirus can be transmitted though mosquito bites',
+        truth: 'To date there has been no information nor evidence to suggest that the new coronavirus could be transmitted by mosquitoes. The new coronavirus is a respiratory virus which spreads primarily through droplets generated when an infected person coughs or sneezes, or through droplets of saliva or discharge from the nose',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mb-mosquito-bite.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'We can use hand dryers to kill the new Coronavirus',
+        truth: 'No. Hand dryers are not effective in killing the new Coronavirus',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbusters-27.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'We can use ultraviolet disinfection lamps to kill the Coronavirus',
+        truth: 'UV lamps should not be used to sterilize hands or other areas of skin as UV radiation can cause skin irritation.',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbusters-31.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Thermal scanners will detect any patients infected with Coronavirus',
+        truth: 'Thermal scanners are effective in detecting people who have developed a fever (i.e. have a higher than normal body temperature) because of infection with the new coronavirus. However, they cannot detect people who are infected but are not yet sick with fever. This is because it takes between 2 and 10 days before people who are infected become sick and develop a fever.',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbusters-25.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Spraying alcohol or chlorine all over your body kill the new coronavirus',
+        truth: 'No. Spraying alcohol or chlorine all over your body will not kill viruses that have already entered your body. Spraying such substances can be harmful to clothes or mucous membranes (i.e. eyes, mouth)',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/mythbusters-33.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Vaccines against pneumonia protect you against the new coronavirus',
+        truth: 'No. Vaccines against pneumonia, such as pneumococcal vaccine and Haemophilus influenza type B (Hib) vaccine, do not provide protection against the new coronavirus. The virus is so new and different that it needs its own vaccine',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/11.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Regularly rinsing your nose with saline help prevent infection with the new coronavirus',
+        truth: 'No. There is no evidence that regularly rinsing the nose with saline has protected people from infection with the new coronavirus',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/23.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Eating garlic help prevent infection with the new coronavirus',
+        truth: 'Garlic is a healthy food that may have some antimicrobial properties. However, there is no evidence from the current outbreak that eating garlic has protected people from the new coronavirus.',
+        link: 'https://www.who.int/images/default-source/health-topics/coronavirus/myth-busters/19.png',
+        mythBustedBy: 'World Health Organization'
+      },
+      {
+        myth: 'Coronavirus originated in a laboratory in China',
+        truth: 'Despite the swathes of internet rumors, there is no evidence that this is the case. In fact, a recent study demonstrates that the virus is a natural product of evolution.',
+        link: 'https://www.medicalnewstoday.com/articles/the-new-coronavirus-was-not-genetically-engineered-study-shows',
+        mythBustedBy: 'Medical News Today'
+      },
+      {
+        myth: 'Getting infected by Coronavirus is a death sentence',
+        truth: 'The data thus far suggests that only around 2.3% of people infected with COVID-19 die from the virus',
+        link: 'http://weekly.chinacdc.cn/en/article/id/e53946e2-c6c4-41e9-9a9b-fea8db1a8f51',
+        mythBustedBy: 'China CDC'
       },
       {
         myth: "If you lose your sense of smell, you have COVID.",
-        truth: "It’s common to temporarily lose one’s sense of smell with many viral infections/allergies. It’s a non-specific symptom that may or may not happen with COVID"
+        truth: "It’s common to temporarily lose one’s sense of smell with many viral infections/allergies. It’s a non-specific symptom that may or may not happen with COVID",
+        link: 'https://twitter.com/FaheemYounus/status/1241812329404211202',
+        mythBustedBy: 'Dr. Faheem Younus'
       },
       {
         myth: "Taking hydroxychloroquine and azithromycin preemptively is a good idea to prevent COVID",
-        truth: "These (experimental) drugs for coronavirus should only be used in selected COVID patients. They can sometimes cause fatal heart rhythm problems plus other side effects."
+        truth: "These (experimental) drugs for coronavirus should only be used in selected COVID patients. They can sometimes cause fatal heart rhythm problems plus other side effects.",
+        link: 'https://twitter.com/FaheemYounus/status/1241812330444476428',
+        mythBustedBy: 'Dr. Faheem Younus'
       },
       {
-        myth: "Using garlic/lemon with hot water/onion in the room will prevent or cure COVID-19",
-        truth: "None of these substances have been scientifically tested against COVID. "
+        myth: 'Coronavirus lives in the throat. So drink lots of water so the virus is pushed into the stomach where the acid will kill it',
+        truth: 'Virus may gain entry via throat but it penetrates into the host cells. You can’t wash it away',
+        link: 'https://www.thehindubusinessline.com/news/variety/this-viral-twitter-thread-is-busting-covid-19-myths/article31098486.ece',
+        mythBustedBy: 'Dr. Faheem Younus'
       },
       {
-        myth: "Our governor has declared a state of emergency. It must mean that we are all dying",
-        truth: "The state of emergency is more of a legal than a medical standard. It allows governments to access more resources (like federal funds) and personnel (like the national guards)."
-      },
-      {
-        myth: "Always change your clothes/shower after coming home. Or you will bring coronavirus to your family.",
-        truth: "Cleanliness is a virtue; paranoia isn't. Let’s not scare people. Our biggest return on investment is in handwashing, staying 6 feet away, avoiding large crowds, etc."
-      },
-      {
-        myth: "The messages I receive are from doctors in China/Italy. Why shouldn’t I believe them? ",
-        truth: "Real doctors publish their research in scientific journals, not on social media. Lots of good research is already published"
-      },
-      {
-        myth: "Coronavirus go away in the summer season. ",
-        truth: "Previous pandemics didn’t follow weather patterns plus as we enter summer, there will be winter in the southern hemisphere. Virus is global."
-      },
-      {
-        myth: "In summer, the virus will spread more due to mosquito bites.",
-        truth: "This infection is spread via respiratory droplets and mosquitoes cannot transmit this virus."
+        myth: 'Hand sanitizers are better than soap and water in killing coronavirus',
+        truth: 'Soap and water actually kills and washes away the virus from skin (it can not penetrate our skin cells)',
+        link: 'https://www.thehindubusinessline.com/news/variety/this-viral-twitter-thread-is-busting-covid-19-myths/article31098486.ece',
+        mythBustedBy: 'Dr. Faheem Younus'
       }
     ];
