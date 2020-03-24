2c13d2e34eca7463d451bcf936aeab2f01e86c36
Harshit Jain <harshit_jain@intuit.com>
2020-03-24 18:23:01 +0530

    remove API call

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export default
+[
+      {
+        myth: "Avoid shipped packages/gas pumps/shopping carts/ATMs or you’ll die.",
+        truth: "Coronavirus surface survival is one thing; that surface causing an infection is another. Wash your hands; live your life"
+      },
+      {
+        myth: " You can catch COVID-19 from ordering takeout food/Chinese food (or the packaging of food).",
+        truth: "COVID-19 is a droplet related infection (like flu) not a food-borne infection (like salmonella etc.). There is no documented COVID risk with take-out food."
+      },
+      {
+        myth: "Going into a sauna for 20 min can kill more than 90% of viruses, including coronavirus. ",
+        truth: "There are no scientific trials to suggest the validity of this claim. On the contrary, saunas can cause pneumonias, folliculitis, etc"
+      },
+      {
+        myth: "If you lose your sense of smell, you have COVID.",
+        truth: "It’s common to temporarily lose one’s sense of smell with many viral infections/allergies. It’s a non-specific symptom that may or may not happen with COVID"
+      },
+      {
+        myth: "Taking hydroxychloroquine and azithromycin preemptively is a good idea to prevent COVID",
+        truth: "These (experimental) drugs for coronavirus should only be used in selected COVID patients. They can sometimes cause fatal heart rhythm problems plus other side effects."
+      },
+      {
+        myth: "Using garlic/lemon with hot water/onion in the room will prevent or cure COVID-19",
+        truth: "None of these substances have been scientifically tested against COVID. "
+      },
+      {
+        myth: "Our governor has declared a state of emergency. It must mean that we are all dying",
+        truth: "The state of emergency is more of a legal than a medical standard. It allows governments to access more resources (like federal funds) and personnel (like the national guards)."
+      },
+      {
+        myth: "Always change your clothes/shower after coming home. Or you will bring coronavirus to your family.",
+        truth: "Cleanliness is a virtue; paranoia isn't. Let’s not scare people. Our biggest return on investment is in handwashing, staying 6 feet away, avoiding large crowds, etc."
+      },
+      {
+        myth: "The messages I receive are from doctors in China/Italy. Why shouldn’t I believe them? ",
+        truth: "Real doctors publish their research in scientific journals, not on social media. Lots of good research is already published"
+      },
+      {
+        myth: "Coronavirus go away in the summer season. ",
+        truth: "Previous pandemics didn’t follow weather patterns plus as we enter summer, there will be winter in the southern hemisphere. Virus is global."
+      },
+      {
+        myth: "In summer, the virus will spread more due to mosquito bites.",
+        truth: "This infection is spread via respiratory droplets and mosquitoes cannot transmit this virus."
+      }
+    ];
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../../components/Header';
 import Card from '../../components/Card';
+import data from '../../assets/data';
 
 const Landing = () => {
-  const [mythData, setMythData] = useState([]);
-  useEffect(() => {
-    fetchAndSetData();
-  }, []);
-
-  const fetchAndSetData = async () => {
-    const response = await fetch('http://demo6902092.mockable.io/coronamythbuster/');
-    const data = await response.json();
-    setMythData(data.data);
-  }
   return (
     <div style={{ backgroundColor: '#f5f5f5' }}>
       <Header />
       {
-        mythData.map((item) => (
+        data.map((item) => (
           <Card
           myth={item.myth}
           truth={item.truth}
